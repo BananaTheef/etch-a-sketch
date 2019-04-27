99017a6f121f1cff7607d7290bb33854c5a0f041
BananaTheef <luben.stoyanov@yahoo.com>
2019-04-28 00:27:03 +0200

    Change CSS for better design of buttons and headline. Improve createGrid function by removing the grid before creating new one. Change color scheme.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,59 +1,64 @@
-const mainContainer = document.querySelector('#main-container');
-const body = document.querySelector('body');
+// Select Elements
+const mainContainer = document.querySelector('.main-container');
+const setGridBtn = document.querySelector('.set-grid-btn');
+setGridBtn.addEventListener('click', () => {
+  clearGrid();
+  createGrid(sideLength = prompt("Choose grid-size:"));
+});
 
-const clearGridBtn = document.createElement('button');
-clearGridBtn.textContent = 'Clear Grid';
-clearGridBtn.addEventListener('click', clearGrid);
-body.appendChild(clearGridBtn);
+// Different Modes
+let flag = 0;
+const darkenBtn = document.querySelector('.darken-btn');
+darkenBtn.addEventListener('click', () => flag = 0);
+const colorfulBtn = document.querySelector('.colorful-btn');
+colorfulBtn.addEventListener('click', () => flag = 1);
 
+// Reset Grid
 function clearGrid()
 {
-  window.location.reload();
+  while (mainContainer.firstChild)
+  {
+    mainContainer.removeChild(mainContainer.firstChild);
+  }
 }
 
-/*function getRandomColor ()
-{
-  let color = '#';
-  let letter = '0123456789ABCDEF';
-
-  for (let i=0; i<6; i++)
-  {
-    color+= letter[Math.floor(Math.random() * 16)];
-  }
-
-  return color;
-}*/
-
-
+// Random Color Change of Grid Cells
 function changeColor()
 {
-  this.style.backgroundColor = /*getRandomColor();*/'rgb(' + Math.floor(Math.random() * 256) + ','
-  + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+  if (this.style.backgroundColor === 'rgb(255, 255, 255)' || flag === 1)
+  {
+    this.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 256) + ','
+    + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+  }
+  else if (flag === 0)
+  {
+    let lightness = parseInt(this.style.filter.match(/\d+/));
+    lightness != 0 ? this.style.filter = `brightness(${lightness - 10}%)` : false;
+  }
 }
 
-function createGrid()
+// Create Grid
+function createGrid(n)
 {
+  // Declaration
   let newDiv;
-  let divClassName;
-  let divIdName;
-  let sideLength = prompt("Choose grid-size:");
-  let gridSize = sideLength**2;
+  let gridSize = n**2;
 
-  mainContainer.style.setProperty('display', 'grid');
-  mainContainer.style.setProperty('grid-template-rows', `repeat(${sideLength}, 1fr)`);
-  mainContainer.style.setProperty('grid-template-columns', `repeat(${sideLength}, 1fr)`);
+  // CSS Grid Setup
+  mainContainer.style.setProperty('grid-template-rows', `repeat(${n}, 1fr)`);
+  mainContainer.style.setProperty('grid-template-columns', `repeat(${n}, 1fr)`);
 
+  // Dynamically Creating Div-Elements
   for(let i=0; i<gridSize; i++)
   {
     newDiv = document.createElement('div');
-    divIdName = 'item' + (i+1);
-    newDiv.setAttribute('id', divIdName);
     newDiv.classList.add('grid-item');
-    //newDiv.textContent = 'Luben';
     newDiv.style.backgroundColor = '#FFFFFF';
-    newDiv.addEventListener('mouseover', changeColor);
+    newDiv.style.filter = `brightness(100%)`;
+    newDiv.addEventListener('mouseover', changeColor, true);
     mainContainer.appendChild(newDiv);
   }
 }
 
-createGrid();
+// Base Grid
+createGrid(sideLength = 16);
